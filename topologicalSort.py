42acfda64c67622ef80a2e21e92a71902454aa44
Shreenidhi <pshreenidhi02@gmail.com>
2024-11-18 20:01:54 -0600

    Commit to git

---
+++ topologicalSort.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+
+graph = {
+    'm': ['q', 'r', 'x'],
+    'n': ['q', 'u'],
+    'o': ['r', 's', 'v'],
+    'p': ['o', 's', 'z'],
+    'q': ['t'],
+    'r': ['u', 'y'],
+    's': ['r'],
+    't': ['x', 'y'],
+    'u': ['t'],
+    'v': ['w', 'z'],
+    'w': [],
+    'x': [],
+    'y': ['v'],
+    'z': []
+}
+
+
+visited = set()
+stack = []
+
+
+def dfs(node):
+    if node not in visited:
+        visited.add(node)  
+        for neighbor in graph[node]:  
+            dfs(neighbor)
+        stack.append(node)  
+
+# Topological sort function
+def topological_sort(graph):
+    for node in graph.keys():
+        if node not in visited:
+            dfs(node)
+    return stack[::-1]  
+
+
+result = topological_sort(graph)
+
+
+print("Topological Sort Order:")
+print(result)
